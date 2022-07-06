27291312a11ba86168cb5dd1bdafacdda23c910b
Daffrmdn <rdaffa506@gmail.com>
2022-07-06 19:51:13 +0700

    comment

--- regist_and_login.py
+++ regist_and_login.py
@@ -2,22 +2,24 @@ from selenium import webdriver
 import time
 
 def regist():
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(10)
-    driver.maximize_window()
-    driver.get("https://exoticls.com/") 
-    time.sleep(2)
-    driver.get("https://exoticls.com/User/login")
-    driver.find_element_by_xpath('/html/body/section/article/div/span[2]/a').click()
-    driver.find_element_by_id("reg_id").send_keys("metropolish")
-    driver.find_element_by_id("email").send_keys("dummys123@gmail.com")
-    driver.find_element_by_id("reg_password").send_keys("reaper12")
-    driver.find_element_by_id("nickName").send_keys("XSR")
-    driver.find_element_by_name("ctl02").click()
-    driver.find_element_by_id("login_id").send_keys("metropolish")
-    driver.find_element_by_id("login_password").send_keys("reaper12")
-    driver.find_element_by_name("ctl01").click() 
-    time.sleep(2)
-    driver.close()
+    driver = webdriver.Chrome() # web driver and browser used
+    driver.maximize_window() # to maximize the size of the web to be tested
+    driver.get("https://exoticls.com/") # the web link that we use as testing 
+    time.sleep(2) # give a time lag of 2 seconds
+    
+    driver.get("https://exoticls.com/User/login") # login web link
+    driver.find_element_by_xpath('/html/body/section/article/div/span[2]/a').click() # enter the register section with the button
+    driver.find_element_by_id("reg_id").send_keys("metropolish") # enter the ID, in the syntax send_keys
+    driver.find_element_by_id("email").send_keys("dummys123@gmail.com") # enter the EMAIL  
+    driver.find_element_by_id("reg_password").send_keys("reaper12") # enter the password
+    driver.find_element_by_id("nickName").send_keys("XSR") # enter the Nickname
+    driver.find_element_by_name("ctl02").click() # button register
+    
+    # login field
+    driver.find_element_by_id("login_id").send_keys("metropolish") # enter the id according to the one you registered earlier
+    driver.find_element_by_id("login_password").send_keys("reaper12") # enter the password according to the one you registered earlier
+    driver.find_element_by_name("ctl01").click() # button login 
+    time.sleep(2) # give a time lag of 2 seconds
+    driver.close() # close window browser
 
 regist()
